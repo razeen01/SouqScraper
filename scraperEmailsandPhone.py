4804f40622557334576c10a1e3e195ffaed100d2
razeen01 <hrma5339@gmail.com>
2025-04-10 16:02:59 +0300

    Update scraperEmailsandPhone.py

--- scraperEmailsandPhone.py
+++ scraperEmailsandPhone.py
@@ -2,9 +2,7 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-allsite = ["https://enghamzasalem.com/",
-           "https://www.ionixxtech.com/", "https://sumatosoft.com", "https://4irelabs.com/", "https://www.leewayhertz.com/",
-           "https://stackoverflow.com", "https://www.vardot.com/en", "http://www.clickjordan.net/", "https://vtechbd.com/"]
+allsite = ["https://companysa.net/"]
 emails = []
 tels = []
 for l in allsite:
